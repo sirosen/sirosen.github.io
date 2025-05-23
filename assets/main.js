6328e4b0952874bad0692a950b62d4f91ed9c0cd
Stephen Rosen <sirosen@globus.org>
2025-05-23 19:32:05 -0400

    Fix post images to make them fluid

---
+++ assets/main.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('#main-content img').forEach(function(elem) {
+      elem.classList.add("img-fluid");
+  });
+});
