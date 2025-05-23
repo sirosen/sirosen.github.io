ff020e7a5565c34950f2223bcb08b0678be209c2
Stephen Rosen <sirosen@globus.org>
2025-05-23 19:35:27 -0400

    Minor JS tidy up

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
+/* highlight with HighlightJS */
+hljs.highlightAll();
+
+/* on-load customization; mark images as fluid */
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('#main-content img').forEach(function(elem) {
       elem.classList.add("img-fluid");
